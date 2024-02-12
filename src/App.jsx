9cde1d0a82eab8674dd09c7f291f8c91477a08eb
René <107271569+renevalderrey@users.noreply.github.com>
2024-02-12 16:42:40 -0300

    Routes
    Cree las rutas, ordené un poco las carpetas del front, cree un archivo por cada ruta faltante

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,20 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import logo from "../img/logo.png"
-import './App.css'
+import Home from "./pages/Home/Home.jsx";
+import About from "./pages/About/About.jsx";
+import Services from "./pages/Services/Services.jsx";
+import Contact from "./pages/Contact/Contact.jsx";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 function App() {
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
